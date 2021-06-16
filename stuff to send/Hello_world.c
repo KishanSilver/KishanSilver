a2330ce6711f0959f9d62e181fad9c44d2a78f63
KishanSilver <85975191+KishanSilver@users.noreply.github.com>
2021-06-16 14:59:51 +0800

    added a hello world file

---
+++ stuff to send/Hello_world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world\n");
+    int i = 72;
+    printf("the number is: %d\n", i);
+    float k = 7.25;
+    printf("the decimal is : %f\n", k);
+    return 0;
+}
