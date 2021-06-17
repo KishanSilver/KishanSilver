90841a0136c449976c8dd5137fd1543e599bbb79
Danial <d.ariff63@gmail.com>
2021-06-17 14:57:24 +0800

    I added a new test file

---
+++ stuff to send/lab_6_1_largest_number.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, num3, num4, num5;
+    printf("Enter 5 numbers:\n");
+
+    printf("Input your first value:" );
+    scanf("%d", &num1);
+
+    printf("Input your second value:" );
+    scanf("%d", &num2);
+
+    printf("Input your third value:" );
+    scanf("%d", &num3);
+
+    printf("Input your forth value:" );
+    scanf("%d", &num4);
+
+    printf("Input your fifth value:" );
+    scanf("%d", &num5);
+    
+    if (num1>=num2 && num1 >= num3 && num1 >= num4 && num1 >= num5)
+        printf("%d is the largest number.", num1);
+
+    else if (num2>=num1 && num2 >= num3 && num2 >= num4 && num2 >= num5)
+        printf("%d is the largest number.", num2);
+
+    else if (num3>=num1 && num3 >= num2 && num3 >= num4 && num1 >= num5)
+        printf("%d is the largest number.", num3);
+
+    else if (num4>=num1 && num4 >= num2 && num4 >= num3 && num4 >= num5)
+        printf("%d is the largest number.", num4);
+    
+    else
+        printf("%d is the largest number.", num5);
+
+    return 0;
+}
